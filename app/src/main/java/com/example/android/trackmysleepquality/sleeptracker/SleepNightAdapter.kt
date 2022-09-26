fefde109b5eb9f482b973db0dfc2106cc1742f24
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-09-27 01:24:05 +0500

    Working on the RecyclerView

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -15,3 +15,40 @@
  */
 
 package com.example.android.trackmysleepquality.sleeptracker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
+
+class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+
+    // We are defining the DATA SOURCE
+    var data = listOf<SleepNight>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     */
+    override fun getItemCount(): Int = data.size
+
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+        holder.textView.text = item.sleepQuality.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
+        return TextItemViewHolder(view)
+    }
+
+
+}
